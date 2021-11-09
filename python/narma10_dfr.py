ca8544f60fb4a47427ccc3bae76e174e88f88ad3
Osaze Shears <oshears@vt.edu>
2021-11-09 15:41:10 -0500

    minor updates to narma10 dfr

--- python/narma10_dfr.py
+++ python/narma10_dfr.py
@@ -18,62 +18,7 @@ def mg(x):
 
     return (a * x) / (b + c * np.power( (d * x), p) )
 
-# def mg(x):
-#     C = 1.33
-#     # p = 6.88
-#     # p = 7
-#     p = 1
-#     b = 0.4
-
-#     return C * x / (1 + np.power(b * x,p)) 
-
-
-def mg_deriv(x):
-
-    a = 2
-    b = 0.8
-    c = 0.2
-    d = 2.1
-    p = 10
-
-
-    return (a * (b - (p - 1) * c * np.power(d * x, p))) / np.power((b + c * np.power( (d * x), p) ),2)
-
-def relu(x):
-    return np.array([np.max(xi,0) for xi in x])
-
-def relu_deriv(x):
-    y = np.zeros(x.size)
-    y[x >= 0] = 1
-    y[x < 0] = 0 
-    return y
-
-# inputs
-x = np.linspace(-1,1,100)
-
-# mg equation characteristics
-y_mg = mg(x)
-y_mg_deriv = mg_deriv(x)
-
-# relu characteristics
-y_relu = relu(x)
-y_relu_deriv = relu_deriv(x)
-
-# plot activation functions
-# plt.subplot(2,2,1)
-# plt.plot(x,y_mg)
-# plt.subplot(2,2,2)
-# plt.plot(x,y_relu)
-# plt.subplot(2,2,3)
-# plt.plot(x,y_mg_deriv)
-# plt.subplot(2,2,4)
-# plt.plot(x,y_relu_deriv)
-# plt.show()
-
-###############################################
-
 init_samples = 200
-# train_samples = 4000
 train_samples = 15000
 
 num_samples = train_samples + init_samples
