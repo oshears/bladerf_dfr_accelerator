c69ad4e6a46aaac568d663836daef648e506aa8b
Osaze Shears <oshears@vt.edu>
2021-11-15 19:05:54 -0500

    separated dfr component into different c++ file

--- intel_hls/hls_float_dfr_spectrum/dfr.cpp
+++ intel_hls/hls_float_dfr_spectrum/dfr.cpp
@@ -1,5 +1,3 @@
-//  Copyright (c) 2021 Intel Corporation                                  
-//  SPDX-License-Identifier: MIT                                          
 #include <stdio.h>
 
 #include "HLS/hls.h"
@@ -7,12 +5,9 @@
 #include "HLS/hls_float.h"
 #include "HLS/hls_float_math.h"
 
-
 #include "dfr.h"
 
 using namespace ihc;
-
-
 
 DFR_FP mackey_glass(DFR_FP x){
 
@@ -60,56 +55,4 @@ component DFR_FP dfr(DFR_FP i_data, DFR_FP q_data) {
   }
 
   return dfr_out;
-}
-
-int main() {
-
-  // define sample counts
-  constexpr int NUM_INIT_SAMPLES = 200;
-  constexpr int NUM_TEST_SAMPLES = 1000;
-  constexpr int NUM_TOTAL_SAMPLES = NUM_INIT_SAMPLES + NUM_TEST_SAMPLES;
-
-  // generate narma10 inputs and outputs
-  printf("Creating input and output data vectors...\n");
-  DFR_FP** u = iq_inputs(NUM_TOTAL_SAMPLES);
-  DFR_FP* y = spectrum_outputs(NUM_TOTAL_SAMPLES);
-
-
-  // get test data vectors
-  printf("Parsing test input and output vectors...\n");
-  // DFR_FP* u_test = get_vector_indexes(u,NUM_INIT_SAMPLES,NUM_TOTAL_SAMPLES);
-  // DFR_FP* y_test = get_vector_indexes(y,NUM_INIT_SAMPLES,NUM_TOTAL_SAMPLES);
-
-  // store test data outputs
-  DFR_FP y_hat_test[NUM_TEST_SAMPLES];
-
-  // reservoir initialization
-  printf("Initializing Reservoir...\n");
-  for(unsigned int i = 0; i < NUM_INIT_SAMPLES; i++) ihc_hls_enqueue_noret(&dfr,u[i][0],u[i][1]);
-  ihc_hls_component_run_all(dfr);
-
-  // reservoir test
-  printf("Testing DFR...\n");
-  for(unsigned int i = 0; i < NUM_TEST_SAMPLES; i++) ihc_hls_enqueue(&y_hat_test[i], &dfr,u[i+NUM_INIT_SAMPLES][0],u[i+NUM_INIT_SAMPLES][1]);
-  ihc_hls_component_run_all(dfr);
-
-  // calculate the NRMSE of the predicted output
-  // DFR_FP nrmse = get_nrmse(y_hat_test,y_test,NUM_TEST_SAMPLES);
-  // printf("Test NRMSE\t= %s\n",nrmse.get_str().c_str());
-
-  // calculate the MSE of the predicted output
-  // DFR_FP mse = get_mse(y_hat_test,y,NUM_TEST_SAMPLES);
-  // printf("Test MSE\t= %s\n",mse.get_str().c_str());
-
-  // calculate accuracy
-  float accuracy = 0;
-  for(int i = 0; i < NUM_TEST_SAMPLES; i++){
-    bool prediction = (y_hat_test[i] >= 0.5);
-    if(prediction == y[i+NUM_INIT_SAMPLES]) accuracy++;
-  }
-  accuracy = accuracy / NUM_TEST_SAMPLES;
-  printf("Test Accuracy = %f\n",accuracy);
-
-  return 0;
-
 }
---
+++ intel_hls/hls_float_dfr_spectrum/main.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+#include "HLS/hls.h"
+#include "HLS/math.h"
+#include "HLS/hls_float.h"
+#include "HLS/hls_float_math.h"
+
+#include "dfr.h"
+
+using namespace ihc;
+
+int main() {
+
+  // define sample counts
+  constexpr int NUM_INIT_SAMPLES = 200;
+  constexpr int NUM_TEST_SAMPLES = 1000;
+  constexpr int NUM_TOTAL_SAMPLES = NUM_INIT_SAMPLES + NUM_TEST_SAMPLES;
+
+  // generate narma10 inputs and outputs
+  printf("Creating input and output data vectors...\n");
+  DFR_FP** u = iq_inputs(NUM_TOTAL_SAMPLES);
+  DFR_FP* y = spectrum_outputs(NUM_TOTAL_SAMPLES);
+
+
+  // get test data vectors
+  printf("Parsing test input and output vectors...\n");
+  // DFR_FP* u_test = get_vector_indexes(u,NUM_INIT_SAMPLES,NUM_TOTAL_SAMPLES);
+  // DFR_FP* y_test = get_vector_indexes(y,NUM_INIT_SAMPLES,NUM_TOTAL_SAMPLES);
+
+  // store test data outputs
+  DFR_FP y_hat_test[NUM_TEST_SAMPLES];
+
+  // reservoir initialization
+  printf("Initializing Reservoir...\n");
+  for(unsigned int i = 0; i < NUM_INIT_SAMPLES; i++) ihc_hls_enqueue_noret(&dfr,u[i][0],u[i][1]);
+  ihc_hls_component_run_all(dfr);
+
+  // reservoir test
+  printf("Testing DFR...\n");
+  for(unsigned int i = 0; i < NUM_TEST_SAMPLES; i++) ihc_hls_enqueue(&y_hat_test[i], &dfr,u[i+NUM_INIT_SAMPLES][0],u[i+NUM_INIT_SAMPLES][1]);
+  ihc_hls_component_run_all(dfr);
+
+  // calculate the NRMSE of the predicted output
+  // DFR_FP nrmse = get_nrmse(y_hat_test,y_test,NUM_TEST_SAMPLES);
+  // printf("Test NRMSE\t= %s\n",nrmse.get_str().c_str());
+
+  // calculate the MSE of the predicted output
+  // DFR_FP mse = get_mse(y_hat_test,y,NUM_TEST_SAMPLES);
+  // printf("Test MSE\t= %s\n",mse.get_str().c_str());
+
+  // calculate accuracy
+  float accuracy = 0;
+  for(int i = 0; i < NUM_TEST_SAMPLES; i++){
+    bool prediction = (y_hat_test[i] >= 0.5);
+    if(prediction == y[i+NUM_INIT_SAMPLES]) accuracy++;
+  }
+  accuracy = accuracy / NUM_TEST_SAMPLES;
+  printf("Test Accuracy = %f\n",accuracy);
+
+  return 0;
+
+}
