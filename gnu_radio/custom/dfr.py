9aa2f14b0cd87a847d612d2626e3f0231c0fca69
Osaze Shears <yoze15@gmail.com>
2022-03-20 11:08:01 -0400

    configuring dfr gnu radio block

--- gnu_radio/custom/dfr.py
+++ gnu_radio/custom/dfr.py
@@ -25,9 +25,8 @@ from PyQt5 import Qt
 from gnuradio import qtgui
 from gnuradio.filter import firdes
 import sip
-from gnuradio import analog
 from gnuradio import blocks
-import numpy
+import pmt
 from gnuradio import digital
 from gnuradio import gr
 from gnuradio.fft import window
@@ -38,7 +37,6 @@ from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio.qtgui import Range, RangeWidget
 from PyQt5 import QtCore
-import dfr_epy_block_1 as epy_block_1  # embedded python block
 import dfr_epy_block_1_0 as epy_block_1_0  # embedded python block
 
 
@@ -82,40 +80,37 @@ class dfr(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 1
-        self.qpsk_constellation = qpsk_constellation = digital.constellation_calcdist([-1-1j, -1+1j, 1+1j, 1-1j, 0], [0, 1, 3, 2, 4],
+        self.qpsk_constellation = qpsk_constellation = digital.constellation_calcdist([-1-1j, -1+1j, 1+1j, 1-1j, 0,0,0,0], [0, 1, 3, 2, 4,5,6,7],
         4, 1, digital.constellation.AMPLITUDE_NORMALIZATION).base()
         self.noise_level = noise_level = 0
 
         ##################################################
         # Blocks
         ##################################################
-        self._noise_level_range = Range(0, 5, 0.01, 0, 200)
-        self._noise_level_win = RangeWidget(self._noise_level_range, self.set_noise_level, "Noise Level", "counter_slider", float, QtCore.Qt.Horizontal)
-        self.top_layout.addWidget(self._noise_level_win)
-        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+        self.qtgui_time_sink_x_0_0_0 = qtgui.time_sink_f(
             10, #size
             samp_rate, #samp_rate
-            "", #name
-            3, #number of inputs
+            'Spectrum Occupancy Prediction', #name
+            1, #number of inputs
             None # parent
         )
-        self.qtgui_time_sink_x_0.set_update_time(3)
-        self.qtgui_time_sink_x_0.set_y_axis(0, 6)
+        self.qtgui_time_sink_x_0_0_0.set_update_time(3)
+        self.qtgui_time_sink_x_0_0_0.set_y_axis(0, 1)
 
-        self.qtgui_time_sink_x_0.set_y_label('Spectrum Energy', "")
+        self.qtgui_time_sink_x_0_0_0.set_y_label('Spectrum Occupancy', "")
 
-        self.qtgui_time_sink_x_0.enable_tags(True)
-        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
-        self.qtgui_time_sink_x_0.enable_autoscale(False)
-        self.qtgui_time_sink_x_0.enable_grid(False)
-        self.qtgui_time_sink_x_0.enable_axis_labels(True)
-        self.qtgui_time_sink_x_0.enable_control_panel(False)
-        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+        self.qtgui_time_sink_x_0_0_0.enable_tags(True)
+        self.qtgui_time_sink_x_0_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0_0_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0_0_0.enable_grid(False)
+        self.qtgui_time_sink_x_0_0_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0_0_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0_0_0.enable_stem_plot(False)
 
 
-        labels = ['Clean Signal', 'Noisy Signal', 'Threshold', 'Signal 4', 'Signal 5',
+        labels = ['Actual', 'Spectrum Occupancy', 'Threshold', 'Signal 4', 'Signal 5',
             'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
-        widths = [3, 4, 3, 1, 1,
+        widths = [3, 3, 3, 1, 1,
             1, 1, 1, 1, 1]
         colors = ['blue', 'red', 'green', 'black', 'cyan',
             'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
@@ -127,19 +122,67 @@ class dfr(gr.top_block, Qt.QWidget):
             -1, -1, -1, -1, -1]
 
 
-        for i in range(3):
+        for i in range(1):
             if len(labels[i]) == 0:
-                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+                self.qtgui_time_sink_x_0_0_0.set_line_label(i, "Data {0}".format(i))
             else:
-                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
-            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
-            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
-            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
-            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
-            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+                self.qtgui_time_sink_x_0_0_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0_0_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0_0_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0_0_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0_0_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0_0_0.set_line_alpha(i, alphas[i])
 
-        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
-        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self._qtgui_time_sink_x_0_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_0_0_win)
+        self.qtgui_time_sink_x_0_0 = qtgui.time_sink_f(
+            10, #size
+            samp_rate, #samp_rate
+            'Spectrum Energy', #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_time_sink_x_0_0.set_update_time(3)
+        self.qtgui_time_sink_x_0_0.set_y_axis(0, 1)
+
+        self.qtgui_time_sink_x_0_0.set_y_label('Spectrum Energy', "")
+
+        self.qtgui_time_sink_x_0_0.enable_tags(True)
+        self.qtgui_time_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0_0.enable_grid(False)
+        self.qtgui_time_sink_x_0_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0_0.enable_stem_plot(False)
+
+
+        labels = ['Input', 'Spectrum Occupancy', 'Threshold', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [3, 3, 3, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [0, 0, 2, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [0, 2, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_0_win)
         self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
             10, #size
             "", #name
@@ -181,27 +224,30 @@ class dfr(gr.top_block, Qt.QWidget):
 
         self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.qwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
+        self._noise_level_range = Range(0, 5, 0.01, 0, 200)
+        self._noise_level_win = RangeWidget(self._noise_level_range, self.set_noise_level, "Noise Level", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._noise_level_win)
         self.epy_block_1_0 = epy_block_1_0.blk()
-        self.epy_block_1 = epy_block_1.blk()
-        self.digital_constellation_encoder_bc_1 = digital.constellation_encoder_bc(qpsk_constellation)
-        self.blocks_add_xx_0 = blocks.add_vcc(1)
-        self.analog_random_source_x_0 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 5, 1000))), True)
-        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, noise_level, 0)
-        self.analog_const_source_x_0 = analog.sig_source_f(0, analog.GR_CONST_WAVE, 0, 0, 0.705)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_cc(1 / (2 ** 11))
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.blocks_file_source_0_0_0 = blocks.file_source(gr.sizeof_float*1, '/home/oshears/Documents/bladeRF/bladerf_dfr_accelerator/gnu_radio/custom/spectrum_data/out_data.bin', True, 0, 0)
+        self.blocks_file_source_0_0_0.set_begin_tag(pmt.PMT_NIL)
+        self.blocks_file_source_0_0 = blocks.file_source(gr.sizeof_float*1, '/home/oshears/Documents/bladeRF/bladerf_dfr_accelerator/gnu_radio/custom/spectrum_data/q_data.bin', True, 0, 0)
+        self.blocks_file_source_0_0.set_begin_tag(pmt.PMT_NIL)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_float*1, '/home/oshears/Documents/bladeRF/bladerf_dfr_accelerator/gnu_radio/custom/spectrum_data/i_data.bin', True, 0, 0)
+        self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_const_source_x_0, 0), (self.qtgui_time_sink_x_0, 2))
-        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
-        self.connect((self.analog_random_source_x_0, 0), (self.digital_constellation_encoder_bc_1, 0))
-        self.connect((self.blocks_add_xx_0, 0), (self.epy_block_1, 0))
-        self.connect((self.blocks_add_xx_0, 0), (self.qtgui_const_sink_x_0, 0))
-        self.connect((self.digital_constellation_encoder_bc_1, 0), (self.blocks_add_xx_0, 0))
-        self.connect((self.digital_constellation_encoder_bc_1, 0), (self.epy_block_1_0, 0))
-        self.connect((self.epy_block_1, 0), (self.qtgui_time_sink_x_0, 1))
-        self.connect((self.epy_block_1_0, 0), (self.qtgui_time_sink_x_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.blocks_file_source_0_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.blocks_file_source_0_0_0, 0), (self.qtgui_time_sink_x_0_0_0, 0))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.epy_block_1_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.qtgui_const_sink_x_0, 0))
+        self.connect((self.epy_block_1_0, 0), (self.qtgui_time_sink_x_0_0, 0))
 
 
     def closeEvent(self, event):
@@ -217,7 +263,8 @@ class dfr(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
+        self.qtgui_time_sink_x_0_0.set_samp_rate(self.samp_rate)
+        self.qtgui_time_sink_x_0_0_0.set_samp_rate(self.samp_rate)
 
     def get_qpsk_constellation(self):
         return self.qpsk_constellation
@@ -230,7 +277,6 @@ class dfr(gr.top_block, Qt.QWidget):
 
     def set_noise_level(self, noise_level):
         self.noise_level = noise_level
-        self.analog_noise_source_x_0.set_amplitude(self.noise_level)
 
 
 
